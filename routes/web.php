7e19ebeac0946da473374f2b8c7487b287a7ea99
MennaELRouby <roubymenna@gmail.com>
2024-01-25 13:29:32 +0200

    create schema & Model & Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\CarController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MessageController;
+use App\Http\Controllers\Admin\TestimonialController;
+use App\Http\Controllers\Admin\UserController;
+
+
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,73 +23,52 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 Route::get('about', function () {
     return view('about');
-});
+})->name('about');
 Route::get('blog', function () {
     return view('blog');
-});
+})->name('blog');
 Route::get('contact', function () {
     return view('contact');
-});
+})->name('contact');
 Route::get('listing', function () {
     return view('listing');
-});
+})->name('listing');
 Route::get('single', function () {
     return view('single');
-});
+})->name('single');
 Route::get('testimonials', function () {
     return view('testimonials');
-});
+})->name('testimonials');
 
 Route::get('main', function () {
     return view('main');
-});
+})->name('main');
 
+Route::fallback(fn () => redirect('main'));
 
+/*****************************Admin Dashboard*********************************/
 Route::prefix('admin')->group(function () {
-    Route::get('addcar', function () {
-        return view('admin/addCar');
-    });
-    Route::get('cars', function () {
-        return view('admin/cars');
-    });
-    Route::get('editcar', function () {
-        return view('admin/editCar');
-    });
-    Route::get('addcategory', function () {
-        return view('admin/addCategory');
-    });
-    Route::get('categories', function () {
-        return view('admin/categories');
-    });
-    Route::get('editcategory', function () {
-        return view('admin/editCategory');
-    });
+    Route::get('addcar', [CarController::class, 'create']);
+    Route::get('cars', [CarController::class, 'index']);
+    Route::get('editcar', [CarController::class, 'edit']);
 
-    Route::get('adduser', function () {
-        return view('admin/addUser');
-    });
-    Route::get('users', function () {
-        return view('admin/users');
-    });
-    Route::get('edituser', function () {
-        return view('admin/editUser');
-    });
-    Route::get('addtestimonials', function () {
-        return view('admin/addTestimonials');
-    });
-    Route::get('testimonials', function () {
-        return view('admin/testimonials');
-    });
-    Route::get('edittestimonial', function () {
-        return view('admin/editTestimonials');
-    });
-    Route::get('messages', function () {
-        return view('admin/messages');
-    });
-    Route::get('showmessages', function () {
-        return view('admin/showMessages');
-    });
+    Route::get('addcategory', [CategoryController::class, 'create']);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('editcategory', [CategoryController::class, 'edit']);
+
+    Route::get('adduser', [UserController::class, 'create']);
+    Route::get('user', [UserController::class, 'index']);
+    Route::get('edituser', [UserController::class, 'edit']);
+
+    Route::get('addtestimonials', [TestimonialController::class, 'create']);
+    Route::get('testimonials', [TestimonialController::class, 'index']);
+    Route::get('edittestimonials', [TestimonialController::class, 'edit']);
+
+    Route::get('messages', [MessageController::class, 'index']);
+    Route::get('showmessage', [MessageController::class, 'show']);
 });
+
+Route::post('car', [CarController::class, 'store']);
